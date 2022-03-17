b9d0244c813b6c529f30233f6d3e2f078670f46b
CodeDummy-03 <sohampal1995@gmail.com>
2022-03-17 23:33:08 +0530

    Add project files.

---
+++ LeetCode/Arrays/DuplicateElementsInAnArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace Leetcode.Arrays
+{
+    public class DuplicateElementsInAnArray
+    {
+        ArrayList inputArray;
+        HashSet<int> duplicateElements;
+        public void getInput()
+        {
+            Console.WriteLine("Enter the number of elements in the array");
+            int maxValues = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the input values one by one");
+            inputArray = new ArrayList();
+            
+            for (int index = 0; index < maxValues; index++)
+            {
+                inputArray.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+        }
+
+        public void sortInputs()
+        {
+            inputArray.Sort();
+        }
+
+        public void getDuplicateElements()
+        {
+            sortInputs();
+            duplicateElements = new HashSet<int>();
+            for (int index = 0; index < inputArray.Count; index++)
+            {
+                int value = Convert.ToInt32(inputArray[index]);
+                inputArray.Remove(value);
+                if(inputArray.Contains(value))
+                {
+                    duplicateElements.Add(value);
+                }
+            }
+
+            foreach (int value in duplicateElements)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// MainProgram contains Main() method for all relevant classes to be tested.
+/// </summary>
+using Leetcode.Arrays;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DuplicateElementsInAnArray ob = new DuplicateElementsInAnArray();
+        ob.getInput();
+        ob.getDuplicateElements();
+    }
+}
